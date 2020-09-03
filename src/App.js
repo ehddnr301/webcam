f709484370eec2315040551d6d18d5815a2a5ad3
ehddnr301 <dy950328@gmail.com>
2020-09-03 21:27:27 +0900

    view update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { VideoFeed } from "./VideoFeed";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const Reset = createGlobalStyle`
+   ${reset};
+   * {
+        box-sizing:border-box;
+    }
+    body{
+      background-color:black;
+    }
+`;
 
 function App() {
   return (
     <>
+      <Reset />
       <div className="App">
         <VideoFeed />
       </div>
--- src/VideoFeed.js
+++ src/VideoFeed.js
@@ -1,8 +1,32 @@
 import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  border: 1px solid #f39c12;
+  border-radius: 20px;
+`;
+
 const Video = styled.video`
   transform: scaleX(-1);
+  width: 60%;
+  height: 80%;
+  padding: 10px;
+  margin-right: 10px;
+`;
+
+const TextBox = styled.div`
+  color: #f39c12;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & > div:not(:last-child) {
+    margin-bottom: 15px;
+  }
 `;
 
 export function VideoFeed() {
@@ -19,5 +43,16 @@ export function VideoFeed() {
     });
   }, [videoEl]);
 
-  return <Video ref={videoEl} />;
+  return (
+    <>
+      <Container>
+        <Video ref={videoEl} />
+        <TextBox>
+          <div>마스크상태</div>
+          <div>착용</div>
+          <div>미착용</div>
+        </TextBox>
+      </Container>
+    </>
+  );
 }
