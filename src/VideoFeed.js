7e1fe08d52b2bdf322aeff4fe34acba504db13b1
ehddnr301 <dy950328@gmail.com>
2020-09-18 22:57:27 +0900

    Feature - Face detect

--- src/VideoFeed.js
+++ src/VideoFeed.js
@@ -1,5 +1,33 @@
-import React, { useRef, useEffect } from "react";
+// export function VideoFeed() {
+//   const videoEl = useRef(null);
+
+//   useEffect(() => {
+//     if (!videoEl) {
+//       return;
+//     }
+//     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+//       let video = videoEl.current;
+//       video.srcObject = stream;
+//       video.play();
+//     });
+//   }, [videoEl]);
+
+//   return (
+//     <>
+//       <Container>
+//         <Video ref={videoEl} />
+//         <TextBox>
+//           <div>마스크상태</div>
+//           <div>착용</div>
+//           <div>미착용</div>
+//         </TextBox>
+//       </Container>
+//     </>
+//   );
+// }
+import React, { useRef, useEffect, useState } from "react";
 import styled from "styled-components";
+import * as faceApi from "face-api.js";
 
 const Container = styled.div`
   display: flex;
@@ -29,24 +57,61 @@ const TextBox = styled.div`
   }
 `;
 
+const Canvas = styled.canvas``;
+
 export function VideoFeed() {
+  const [initial, setInitial] = useState(false);
+  const canvasRef = useRef();
   const videoEl = useRef(null);
 
-  useEffect(() => {
-    if (!videoEl) {
-      return;
-    }
+  const startVideo = () => {
     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
       let video = videoEl.current;
-      video.srcObject = stream;
+      videoEl.current.srcObject = stream;
       video.play();
     });
-  }, [videoEl]);
+  };
+  const handleVideo = async () => {
+    setInterval(async () => {
+      const detections = await faceApi
+        .detectAllFaces(videoEl.current, new faceApi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+      console.log(detections);
+    }, 500);
+  };
+
+  useEffect(() => {
+    const loadModels = async () => {
+      console.log("models");
+      const MODEL_URL = process.env.PUBLIC_URL + "/models";
+      setInitial(true);
+      Promise.all([
+        faceApi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceApi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceApi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceApi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]).then(startVideo);
+    };
+
+    loadModels();
+
+    // if (!videoEl) {
+    //   return;
+    // }
+    // navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+    //   let video = videoEl.current;
+    //   video.srcObject = stream;
+    //   video.play();
+    // });
+    // }, [videoEl]);
+  }, []);
 
   return (
     <>
       <Container>
-        <Video ref={videoEl} />
+        <Video ref={videoEl} autoPlay muted onPlay={handleVideo} />
+        <Canvas ref={canvasRef} />
         <TextBox>
           <div>마스크상태</div>
           <div>착용</div>
